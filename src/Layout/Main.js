db145413edf6c71b5d49f4e8656ec3e2efba5fff
m7real <masayeedreal@gmail.com>
2022-11-08 10:46:32 +0600

    layout & router added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
 
 const Main = () => {
   return (
     <div>
-      <h2>Main</h2>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
