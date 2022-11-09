6f67e97a6ed88ad1d0c541cc472adf00a806b2f8
m7real <masayeedreal@gmail.com>
2022-11-09 09:05:12 +0600

    react photo view added

--- src/Pages/Services/ServiceCard/ServiceCard.js
+++ src/Pages/Services/ServiceCard/ServiceCard.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
   const { _id, name, img, description, price } = service;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={img} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{description?.length > 100 ? description.slice(0, 100) + "..." : description}</p>
-        <div className="mt-3 card-actions justify-between">
-          <h3 className="text-xl font-semibold">Price: ${price}</h3>
-          <Link to={`/services/${_id}`} className="btn btn-primary">
-            View Details
-          </Link>
+    <PhotoProvider>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <PhotoView src={img}>
+            <img src={img} alt="Shoes" title="click to view" />
+          </PhotoView>
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{description?.length > 100 ? description.slice(0, 100) + "..." : description}</p>
+          <div className="mt-3 card-actions justify-between">
+            <h3 className="text-xl font-semibold">Price: ${price}</h3>
+            <Link to={`/services/${_id}`} className="btn btn-primary">
+              View Details
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </PhotoProvider>
   );
 };
 
